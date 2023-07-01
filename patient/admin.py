0775637090cc807510bb2119bbe96c6ddb1c74c3
JManan <manan.jain.0912@gmail.com>
2023-07-01 20:19:46 +0530

    .

--- patient/admin.py
+++ patient/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Patient, Prescription
+from .models import Patient, Prescription
 
 admin.site.register0(Patient) 
 admin.site.register0(Prescription) 

08a981016f75483b0cf294b57672f300d69b7297
JManan <manan.jain.0912@gmail.com>
2023-07-01 17:15:41 +0530

    vercel deploy fix

--- backend/settings.py
+++ backend/settings.py
@@ -1,21 +1,13 @@
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-n-ppxvwtgc48(b$(f1lhm64vzks(qcjir6!$)y(t52o2)i*&h8"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
-# Application definition
+ALLOWED_HOSTS = ["*", "localhost", ".vercel.app"]
 
 INSTALLED_APPS = [
     "doc",
@@ -46,6 +38,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -83,10 +76,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -97,12 +86,6 @@ USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- doc/urls.py
+++ doc/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from doc import views as doc_views
 
 urlpatterns = [
-    path("", doc_views.index, name="index")
+    path("", doc_views.index, name="index"),
     path("scrape/", doc_views.scrape, name="scrape")
 ]
